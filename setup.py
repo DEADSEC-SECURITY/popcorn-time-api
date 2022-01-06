a2aa5ffa3463c6de2e828640535592e2f0113f4e
DEADSEC-SECURITY <amng835@gmail.com>
2022-01-06 19:04:50 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import pathlib
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text(encoding='utf8')
+
+setup(
+    name='popcorn-time-api',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Interact with the popcorntime API with python',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='DeadSec-Security',
+    author_email='amng835@gmail.com',
+    url='https://github.com/DEADSEC-SECURITY/popcorn-time-api',
+    keywords=[
+        'popcorn',
+        'popcorntime',
+        'popcorn-time',
+        'popcorn api',
+        'popcorntimeapi',
+        'popcorntime-api',
+        'popcorn-time-api'
+    ],
+    license='MIT',
+    install_requires=[
+        'requests==2.27.1'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.2'],
+    python_requires='>=3.7'
+)
